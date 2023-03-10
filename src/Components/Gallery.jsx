795b894bd175d9a234b0524ad79b632a5d9a818c
yeexng <118996751+yeexng@users.noreply.github.com>
2023-03-10 16:43:52 +0000

    Updated but not working fetch

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -3,72 +3,76 @@ import { useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/esm/Container";
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
-import Spinner from "react-bootstrap/esm/Spinner"
-import Alert from "react-bootstrap/esm/Alert"
+import Spinner from "react-bootstrap/esm/Spinner";
+import Alert from "react-bootstrap/esm/Alert";
 
 const Gallery = (props) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIserror] = useState(false);
 
-    const [movies,setMovies] = useState([])
-    const [isLoading,setIsLoading] = useState (true)
-    const [isError,setIserror] = useState (false)
+  const fetchMovies = async () => {
+    try {
+      let response = await fetch(
+        "https://long-plum-chimpanzee-tie.cyclic.app/" + props.query
+      );
+      console.log(response);
+      if (response.ok) {
+        let data = await response.json();
+        console.log(data);
+        setMovies(data.Search);
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+        setIserror(true);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      setIserror(true);
+    }
+  };
 
-    const fetchMovies = async () => {
-        try{
-            let response = await fetch("http://www.omdbapi.com/?apikey=bda9b329&s=" + props.query)
-            console.log(response)
-             if (response.ok){
-                let data = await response.json()
-                console.log(data)
-                setMovies(data.Search)
-                setIsLoading(false)
-            } else{
-                setIsLoading(false)
-                setIserror(true)
-            }
-        }catch(error){
-        console.log(error)
-        setIsLoading(false)
-        setIserror(true)
-    }}
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
-    useEffect(()=>{
-        fetchMovies()
-    },[])
+  return (
+    <Container fluid>
+      {isLoading && ( // isLoading is true or false
+        <Spinner animation="border" variant="success" />
+      )}
+      {isError && <Alert variant="danger">Aww snap, we got an error!😨</Alert>}
 
+      <Col sm={12} md={12} lg={12}>
+        <h5 className="text-light mt-5">{props.genre}</h5>
+      </Col>
 
-    return(
-        <Container fluid>
+      <Row>
+        {movies
+          ? movies.slice(0, 6).map((res) => {
+              return (
+                <Col
+                  sm={5}
+                  md={3}
+                  lg={3}
+                  xl={2}
+                  key={res.imdbID}
+                  className="p-0"
+                  onClick={() => {
+                    navigate(`/movie-details/${res.imdbID}`);
+                  }}
+                >
+                  <img className="img-fluid" src={res.Poster} alt={res.Title} />
+                </Col>
+              );
+            })
+          : null}
+      </Row>
+    </Container>
+  );
+};
 
-            {isLoading && ( // isLoading is true or false
-            <Spinner animation="border" variant="success" />
-            )}
-            {isError && (
-            <Alert variant="danger">Aww snap, we got an error!😨</Alert>
-            )}
-
-
-            <Col sm={12} md={12} lg={12}><h5 className="text-light mt-5">{props.genre}</h5></Col>
-            
-            <Row>
-                
-
-                {movies ? movies.slice(0,6).map((res)=>{
-                    return (
-                        <Col sm={5} md={3} lg={3} xl={2} key={res.imdbID} className="p-0" onClick={()=>{navigate(`/movie-details/${res.imdbID}`)}}
-                        >
-                            <img className="img-fluid" src={res.Poster} alt={res.Title} />
-                        </Col>
-                    )
-
-                }): null }
-
-
-            </Row>
-        </Container>
-    )
-    
-}
-
-export default Gallery
+export default Gallery;
